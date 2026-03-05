1deeac7dffc83cc1a04ba9cff42ed06a5b13e7ba
Kanje <rowyneo09@gmail.com>
2026-03-05 22:27:38 +0800

    fix: I fixed the functions and syntaxes.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useMemo,useState } from 'react'
+import type { Product } from "../src/Types/index";
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const PRODUCTS: Product[] = [
+  {id: 1, name: "Pizza", price: 10, quantity: 5},
+  {id: 2, name: "Burger", price: 12, quantity: 3},
+];
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+export default function App() {
+  const [cart, setCart] = useState<Product[]>([]);
+    
+  function addToCart(product: Product) {
+    setCart((prev) => {
+      const found = prev.find((i) => i.id === product.id);
+      if (found) {
+        return prev.map((i) =>
+          i.id === product.id ? { ...i, quantity: i.quantity + 1 } : i
+        );
+      }
+      return [...prev, { ...product, quantity: 1 }];
+    });
+  }
+   function changequantity(id: number, delta: number) {
+    setCart((prev) =>
+      prev
+        .map((i) => (i.id === id ? { ...i, quantity: i.quantity + delta } : i))
+        .filter((i) => i.quantity > 0)
+    );
+  }
+
+  const total = useMemo(
+    () => cart.reduce((sum, item) => sum + item.price * item.quantity, 0),
+    [cart]
+  );
+
+  return(
+    <div className="Top">
+      <h2>TRY LANG</h2>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
